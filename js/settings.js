bc634f58997a3976d24f7a4028b0750eee776334
jacqueslelezard <jacques.le.lezard@gmail.com>
2014-12-13 12:16:53 +0100

    settings json

---
+++ js/settings.js
@@ -0,0 +1,26 @@
+var dinosaures = [
+	{
+	            id: "stego",
+	            name: "Stégosaure",
+	            niveaux: [1,2] ,
+	},
+	{
+	            id: "velo",
+	            name: "Vélociraptor",
+	            niveaux: [2,3] ,
+	},
+	{
+	            id: "diplo",
+	            name: "Diplodocus",
+	            niveaux: [1,2,3 ] ,
+	},
+]
+
+var niveau=[
+	{
+		id: 1,
+		background:"",
+		foreground:"",
+		dinosaures: ["stego", "diplo"]
+	}
+]
