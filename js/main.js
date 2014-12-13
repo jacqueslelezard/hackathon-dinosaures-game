9e1b5a15f65ecd9ff97e631a4f8e827f73c1e180
Vivien Ripoche <vivienripoche@MacBook-Air-de-Vivien.local>
2014-12-13 13:13:05 +0100

    Main file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var Menu = {
+  color: '#ffffff',
+  init: function init(settings) {
+    this.settings = settings;
+    this.display();
+  },
+  display: function () {
+  }
+}
+
+$(function(){
+  Menu.init();
+})
