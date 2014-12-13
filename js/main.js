9651555e19e401250927ff4c7547d6cb4a8adf42
jacqueslelezard <jacques.le.lezard@gmail.com>
2014-12-13 14:29:29 +0100

    menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var currentDino="aucun";
+var currentLevel=0;
+
+
 var Menu = {
   color: '#ffffff',
   init: function init() {
@@ -6,10 +10,20 @@ var Menu = {
   },
   display: function () {
   	for(var dino in settings["dinosaures"]){
-  		$('section .menu').append("<section class='menu-dino'>"+settings["dinosaures"][dino]['name']+"</section>");
+  		$('section .menu').append('<div class="menu-dino" id=' + settings.dinosaures[dino].id + '>' + settings.dinosaures[dino].name + '</div>');//.append("<section class='menu-dino' onclick='currentDino="+settings['dinosaures'][dino]['id']+"'>"+settings['dinosaures'][dino]['name']+"</section>");
   	}
+  	setTimeout(function(){
+  		$('.menu-dino').on('click', function(){
+  			currentDino = $(this).attr('id');
+  		});
+  	}, 0);
   }
 }
+
+$("section .menu").click(function(){
+	console.log(currentDino);
+	$("section .menu").hide(300);
+})
 
 $(function(){
   Menu.init();
