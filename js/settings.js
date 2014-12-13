be1d03a00e5d66c86e94ab064ac157405452c2fd
jacqueslelezard <jacques.le.lezard@gmail.com>
2014-12-13 19:19:42 +0100

    images et gestion des niveaux

--- js/settings.js
+++ js/settings.js
@@ -15,7 +15,13 @@ settings.dinosaures= [
   id: "diplo",
   name: "Diplodocus",
   //niveaux: [1,2,3 ] ,
-}];
+},
+{
+  id: "liopleu",
+  name: "Liopleusaure",
+  //niveaux: [1,2,3 ] ,
+},
+];
 
 settings.animations = {
   //fly:[{x:100, y:-100}, {x:500}, {x:600, y:0}],
@@ -36,6 +42,37 @@ settings.levels = [
 		foreground:"",
 		dinosaures: [0, 2],
 		dinosauresMove: ['fly', 'fly'],
-		winners: [0]
+		winners: [2],
+		help: "et tente de parcourir les plaines du Trias."
+	},
+	{
+		id: 2,
+		name:"Jurassique",
+		background:"",
+		foreground:"",
+		dinosaures: [1, 2],
+		dinosauresMove: ['fly', 'fly'],
+		winners: [2],
+		help: "et tente de traverser la forêt du Jurassique."
+	},
+	{
+		id: 3,
+		name:"Crétacé Inférieur",
+		background:"",
+		foreground:"",
+		dinosaures: [1, 2],
+		dinosauresMove: ['fly', 'fly'],
+		winners: [2],
+		help: "et tente d'arriver sur l'autre rive de la mer du Crétacé."
+	},
+	{
+		id: 4,
+		name:"Crétacé Supérieur",
+		background:"",
+		foreground:"",
+		dinosaures: [1, 2],
+		dinosauresMove: ['fly', 'fly'],
+		winners: [2],
+		help: "et tente d'atteindre les hautes falaises."
 	}
 ];
